3d637c6ef7f7ad1730455b2bec5c3563939c72d3
Miles Richardson <miles@splitgraph.com>
2022-06-20 17:35:14 +0100

    Stub `BaseDb` generic `import` function and plugin map

--- packages/base-db/index.ts
+++ packages/base-db/index.ts
@@ -2,3 +2,41 @@ export const helloWorld = () => {
   console.log("Hello world from base-db");
   return "hello world";
 };
+
+export interface Db<PluginMap extends Record<string, unknown>> {
+  importData: <
+    PluginName extends keyof PluginMap,
+    SourceOptions extends Record<PropertyKey, unknown>,
+    DestOptions extends Record<PropertyKey, unknown>,
+    ImportResultShape extends Record<PropertyKey, unknown>,
+    ImportErrorShape extends Record<PropertyKey, unknown>
+  >(
+    plugin: PluginName,
+    source: SourceOptions,
+    dest: DestOptions
+  ) => Promise<{
+    response: ImportResultShape | null;
+    error: ImportErrorShape | null;
+  }>;
+}
+
+export abstract class BaseDb<PluginMap extends Record<string, unknown>>
+  implements Db<PluginMap>
+{
+  // constructor(opts: DbOptions) {}
+
+  abstract importData<
+    PluginName extends keyof PluginMap,
+    SourceOptions extends Record<PropertyKey, unknown>,
+    DestOptions extends Record<PropertyKey, unknown>,
+    ImportResultShape extends Record<PropertyKey, unknown>,
+    ImportErrorShape extends Record<PropertyKey, unknown>
+  >(
+    plugin: PluginName,
+    source: SourceOptions,
+    dest: DestOptions
+  ): Promise<{
+    response: ImportResultShape | null;
+    error: ImportErrorShape | null;
+  }>;
+}
--- packages/db-splitgraph/db-splitgraph.ts
+++ packages/db-splitgraph/db-splitgraph.ts
@@ -1,4 +1,49 @@
-import { helloWorld } from "@madatdata/base-db";
+import { BaseDb, helloWorld } from "@madatdata/base-db";
+
+type CSVPlugin = {
+  sourceOptions: {
+    url: string;
+  };
+  destOptions: {
+    tableName: string;
+  };
+  resultShape: {
+    success: true;
+  };
+  errorShape: {
+    success: false;
+  };
+};
+
+type SplitgraphPluginMap = {
+  csv: CSVPlugin;
+};
+
+class DbSplitgraph extends BaseDb<SplitgraphPluginMap> {
+  async importData<
+    PluginName extends keyof SplitgraphPluginMap,
+    SourceOptions = SplitgraphPluginMap[PluginName]["sourceOptions"],
+    DestOptions = SplitgraphPluginMap[PluginName]["destOptions"],
+    ImportResultShape = SplitgraphPluginMap[PluginName]["resultShape"],
+    ImportErrorShape = SplitgraphPluginMap[PluginName]["errorShape"]
+  >(plugin: PluginName, source: SourceOptions, dest: DestOptions) {
+    if (plugin === "csv") {
+      return Promise.resolve({
+        response: {
+          success: true,
+        } as unknown as ImportResultShape,
+        error: null,
+      });
+    } else {
+      return Promise.resolve({
+        response: null,
+        error: {
+          success: false,
+        } as unknown as ImportErrorShape,
+      });
+    }
+  }
+}
 
 export const makeDb = () => {
   return {
