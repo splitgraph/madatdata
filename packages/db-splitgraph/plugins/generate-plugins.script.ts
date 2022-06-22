21e6b973905860a8fea137ade66df1828d361ea7
Miles Richardson <miles@splitgraph.com>
2022-06-22 03:51:28 +0100

    Delete hardcoded JSON file, actually fetch plugins from gql API

--- packages/db-splitgraph/plugins/generate-plugins.script.ts
+++ packages/db-splitgraph/plugins/generate-plugins.script.ts
@@ -1,7 +1,7 @@
 import { compile } from "json-schema-to-typescript";
 import path from "path";
 import { writeFile, mkdir } from "fs/promises";
-import allPlugins from "./all-plugin-schemas.json";
+import { request, gql } from "graphql-request";
 
 if (!import.meta.url.startsWith("file://")) {
   throw new Error("unexpected import.meta.url (doesn't start with file://)");
@@ -11,7 +11,9 @@ const thisSourceFileDir = path.dirname(new URL(import.meta.url).pathname);
 const targetDir = path.join(thisSourceFileDir, "importers", "generated");
 
 const generateTypes = async () => {
-  for (let plugin of allPlugins.data.externalPlugins) {
+  const allPlugins = await fetchSchemas();
+
+  for (let plugin of allPlugins.externalPlugins) {
     log("generateTypes:", plugin.pluginName);
     let pluginTargetDir = path.join(targetDir, plugin.pluginName);
 
@@ -45,6 +47,37 @@ const generateTypes = async () => {
     // log("write barrel:", fdir(barrelFile), "\n");
     // await writeFile(barrelFile, barrelExports.join("\n"));
   }
+};
+
+const fetchSchemas = async () => {
+  const endpoint = `https://api.splitgraph.com/gql/cloud/unified/graphql`;
+  const query = gql`
+    fragment PluginBase on ExternalPlugin {
+      pluginName
+      name
+      description
+      supportsMount
+      supportsSync
+      supportsLoad
+      marketingSlug
+      canonicalDataSourceName
+      paramsSchema
+      credentialsSchema
+      tableParamsSchema
+    }
+
+    fragment Plugin on ExternalPlugin {
+      ...PluginBase
+    }
+
+    query GetAllPlugins {
+      externalPlugins {
+        ...Plugin
+      }
+    }
+  `;
+
+  return request(endpoint, query);
 };
 
 const run = async () => {
