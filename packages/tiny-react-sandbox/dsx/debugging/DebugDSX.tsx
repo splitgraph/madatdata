804ea2fcd7398394e588940527611222295e40f4
Miles Richardson <miles@splitgraph.com>
2022-07-04 17:17:23 +0100

    Add basic `useSql` hook
    This needs to use the same function overloading as a client
    implementation, perhaps indicating that it should itself be
    a client, e.g. `client-react`, or else there should be some
    tighter coupling between context + class and hook + method

--- packages/tiny-react-sandbox/dsx/debugging/DebugDSX.tsx
+++ packages/tiny-react-sandbox/dsx/debugging/DebugDSX.tsx
@@ -1,3 +1,36 @@
+import { SqlProvider, useSql } from "../DSX";
+
+const DSXQuery = () => {
+  const { loading, error, response } = useSql<{ origin_airport: string }>(
+    `SELECT
+            "origin_airport",
+            "destination_airport",
+            "origin_city",
+            "destination_city",
+            "passengers",
+            "seats",
+            "flights",
+            "distance",
+            "fly_month",
+            "origin_pop",
+            "destination_pop",
+            "id"
+        FROM
+            "splitgraph/domestic_us_flights:latest"."flights"
+        LIMIT ${"100"};`
+  );
+
+  return (
+    <pre style={{ minWidth: "100%", minHeight: 500 }}>
+      {JSON.stringify({ loading, error, response }, null, 2)}
+    </pre>
+  );
+};
+
 export const DebugDSX = () => {
-  return <h1>Debug DSX</h1>;
+  return (
+    <SqlProvider clientOptions={{ credential: null }}>
+      <DSXQuery />
+    </SqlProvider>
+  );
 };
