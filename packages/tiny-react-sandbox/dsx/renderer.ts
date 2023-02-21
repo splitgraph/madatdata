ce477ec71318dbb9346b07f150f08214cf3dcfec
Miles Richardson <miles@splitgraph.com>
2023-02-21 14:35:59 -0500

    Print out debugging info as each node is rendered (wip, stash for later)

--- packages/tiny-react-sandbox/dsx/renderer.ts
+++ packages/tiny-react-sandbox/dsx/renderer.ts
@@ -1,8 +1,56 @@
 import type { ReactNode } from "react";
 import ReactReconciler from "react-reconciler";
 
+const funcCallTable: Record<string, { calls: number; args: unknown[][] }> = {};
+
+const describeFuncCall = (
+  funcName: keyof typeof funcCallTable,
+  funcIter: number,
+  ...funcArgs: unknown[]
+) => {
+  console.log(`    ${funcIter}    ${funcName}(`);
+
+  for (const arg of funcArgs) {
+    console.log(
+      "            ",
+      arg,
+      // add jitter to prevent devtools from combining two identical lines
+      "," + Array(Math.floor(Math.random() * 10) + 2).join(" ")
+    );
+  }
+
+  console.log("         )");
+};
+
+// const printPostRunDebuggingInfo = () => {
+//   for (const funcName in funcCallTable) {
+//     const funcMeta = funcCallTable[funcName];
+
+//     console.log(`%c${funcName}: ${funcMeta.calls}`, "font-weight:bold;");
+
+//     funcMeta.args.forEach((iterArgs, iterI) => {
+//       describeFuncCall(funcName, iterI, ...iterArgs);
+//     });
+//   }
+// };
+
 const notImplemented = (funcName: string, ...args: unknown[]) => {
-  console.warn(`NOT IMPLEMENTED[${funcName}]    `, ...args);
+  if (!(funcName in funcCallTable)) {
+    funcCallTable[funcName] = {
+      calls: 0,
+      args: [],
+    };
+  }
+
+  const funcMeta = funcCallTable[funcName];
+  funcMeta.calls += 1;
+  funcMeta.args.push([...args]);
+
+  describeFuncCall(funcName, funcMeta.calls, ...args);
+
+  // console.log(`%c> ${funcName} (${funcMeta.calls})`, "font-weight:bold;");
+  // args.forEach((arg, argI) => console.log("    arg[", argI, "]=", arg));
+
   // notImplemented(...arguments, ...args)
 };
 
@@ -13,89 +61,92 @@ const reconciler = ReactReconciler({
   },
   supportsPersistence: false,
   createTextInstance: function (
-    text: string,
-    rootContainer: unknown,
-    hostContext: unknown,
-    internalHandle: any
+    _text: string,
+    _rootContainer: unknown,
+    _hostContext: unknown,
+    _internalHandle: any
   ): unknown {
     notImplemented("createTextInstance(->undefined)", ...arguments);
     return undefined;
   },
-  appendInitialChild: function (parentInstance: unknown, child: unknown): void {
+  appendInitialChild: function (
+    _parentInstance: unknown,
+    _child: unknown
+  ): void {
     notImplemented("appendInitialChild", ...arguments);
   },
   finalizeInitialChildren: function (
-    instance: unknown,
-    type: unknown,
-    props: unknown,
-    rootContainer: unknown,
-    hostContext: unknown
+    _instance: unknown,
+    _type: unknown,
+    _props: unknown,
+    _rootContainer: unknown,
+    _hostContext: unknown
   ): boolean {
     notImplemented("finalizeInitialChildren(->undefined)", ...arguments);
     return false;
   },
   prepareUpdate: function (
-    instance: unknown,
-    type: unknown,
-    oldProps: unknown,
-    newProps: unknown,
-    rootContainer: unknown,
-    hostContext: unknown
+    _instance: unknown,
+    _type: unknown,
+    _oldProps: unknown,
+    _newProps: unknown,
+    _rootContainer: unknown,
+    _hostContext: unknown
   ): unknown {
     notImplemented("prepareUpdate(->undefined)", ...arguments);
     return undefined;
   },
-  shouldSetTextContent: function (type: unknown, props: unknown): boolean {
+  shouldSetTextContent: function (_type: unknown, _props: unknown): boolean {
     notImplemented("shouldSetTextContent(->false)", ...arguments);
     return false;
   },
-  getRootHostContext: function (rootContainer: unknown): unknown {
+  getRootHostContext: function (_rootContainer: unknown): unknown {
     notImplemented("getRootHostContext(->undefined)", ...arguments);
     return undefined;
   },
   getChildHostContext: function (
-    parentHostContext: unknown,
-    type: unknown,
-    rootContainer: unknown
+    _parentHostContext: unknown,
+    _type: unknown,
+    _rootContainer: unknown
   ): unknown {
     notImplemented("getChildHostContext(->undefined)", ...arguments);
     return undefined;
   },
-  getPublicInstance: function (instance: unknown): unknown {
+  getPublicInstance: function (_instance: unknown): unknown {
     notImplemented("getPublicInstance(->undefined)", ...arguments);
     return undefined;
   },
   prepareForCommit: function (
-    containerInfo: unknown
+    _containerInfo: unknown
   ): Record<string, any> | null {
     notImplemented("prepareForCommit(->{})", ...arguments);
     return {};
   },
-  resetAfterCommit: function (containerInfo: unknown): void {
+  resetAfterCommit: function (_containerInfo: unknown): void {
     notImplemented("resetAfterCommit", ...arguments);
   },
-  preparePortalMount: function (containerInfo: unknown): void {
+  preparePortalMount: function (_containerInfo: unknown): void {
     notImplemented("preparePortalMount", ...arguments);
   },
   scheduleTimeout: function (
-    fn: (...args: unknown[]) => unknown,
-    delay?: number | undefined
+    _fn: (...args: unknown[]) => unknown,
+    _delay?: number | undefined
   ): unknown {
     notImplemented("scheduleTimeout(->undefined)", ...arguments);
     return undefined;
   },
-  cancelTimeout: function (id: unknown): void {
+  cancelTimeout: function (_id: unknown): void {
     notImplemented("cancelTimeout", ...arguments);
   },
   noTimeout: undefined,
-  isPrimaryRenderer: false,
+  isPrimaryRenderer: true,
   getCurrentEventPriority: function (): number {
     notImplemented("getCurrentEventPriority(->0)", ...arguments);
     // Totally made up number, idk what Lane is
     return 0;
   },
   getInstanceFromNode: function (
-    node: any
+    _node: any
   ): ReactReconciler.Fiber | null | undefined {
     notImplemented("getInstanceFromNode(->null)", ...arguments);
     return null;
@@ -106,27 +157,26 @@ const reconciler = ReactReconciler({
   afterActiveInstanceBlur: function (): void {
     notImplemented("afterActiveInstanceBlur", ...arguments);
   },
-  prepareScopeUpdate: function (scopeInstance: any, instance: any): void {
+  prepareScopeUpdate: function (_scopeInstance: any, _instance: any): void {
     notImplemented("prepareScopeUpdate", ...arguments);
   },
-  getInstanceFromScope: function (scopeInstance: any): unknown {
+  getInstanceFromScope: function (_scopeInstance: any): unknown {
     notImplemented("getInstanceFromScope(->null)", ...arguments);
     return null;
-    // notImplemented(...arguments, "Function not implemented: getInstanceFromScope");
   },
-  detachDeletedInstance: function (node: unknown): void {
+  detachDeletedInstance: function (_node: unknown): void {
     notImplemented("detachDeletedInstance", ...arguments);
   },
   supportsHydration: false,
-  clearContainer: function (container) {
+  clearContainer: function (_container) {
     notImplemented("clearContainer(->null)", ...arguments);
     return null;
   },
-  appendChildToContainer: function (container, child) {
+  appendChildToContainer: function (_container, _child) {
     notImplemented("appendChildToContainer(->null)", ...arguments);
     return null;
   },
-  removeChildFromContainer: function (container, child) {
+  removeChildFromContainer: function (_container, _child) {
     notImplemented("removeChildFromContainer(->null)", ...arguments);
     return null;
   },
@@ -148,4 +198,6 @@ export const render = (element: ReactNode, container: Container) => {
     null
   );
   reconciler.updateContainer(element, root, null, null);
+
+  // printPostRunDebuggingInfo();
 };
--- packages/tiny-react-sandbox/pages/dsx-renderer.tsx
+++ packages/tiny-react-sandbox/pages/dsx-renderer.tsx
@@ -1,10 +1,90 @@
+import type React from "react";
 import { render } from "../dsx/renderer";
 
 // NOTE: This file can be executed in the browser at /dsx-renderer.html
 //       OR via the command line with node www/dist/dsx-renderer.js
 
+const DataContext = ({ children }: React.PropsWithChildren<{}>) => {
+  return <div className="data-context">{children}</div>;
+};
+
+const MountedTable = ({
+  children,
+  sourcePlugin,
+  sourceOpts,
+  destOpts,
+}: React.PropsWithChildren<{
+  sourcePlugin: string;
+  sourceOpts: {
+    fileURL: string;
+  };
+  destOpts: {
+    tableName: string;
+  };
+}>) => {
+  return (
+    <div
+      className="mounted-table"
+      data-sourcePlugin={sourcePlugin}
+      data-destOpts-tableName={destOpts.tableName}
+      data-sourceOpts-fileURL={sourceOpts.fileURL}
+    >
+      {children}
+    </div>
+  );
+};
+
+const SnapshottedTable = ({
+  children,
+  sourcePlugin,
+  sourceOpts,
+  destOpts,
+}: React.PropsWithChildren<{
+  sourcePlugin: string;
+  sourceOpts: {
+    fileURL: string;
+  };
+  destOpts: {
+    tableName: string;
+  };
+}>) => {
+  return (
+    <div
+      className="snapshotted-table"
+      data-sourcePlugin={sourcePlugin}
+      data-destOpts-tableName={destOpts.tableName}
+      data-sourceOpts-fileURL={sourceOpts.fileURL}
+    >
+      {children}
+    </div>
+  );
+};
+
+const QueryTable = ({ children }: React.PropsWithChildren<{}>) => {
+  return <div className="query-table">{children}</div>;
+};
+
 const DsxThingRoot = () => {
-  return <div>Helo</div>;
+  return (
+    <div id="dsx-root">
+      <DataContext>
+        <MountedTable
+          sourcePlugin="csv"
+          sourceOpts={{ fileURL: "https://link-to-live-csv.s3.test" }}
+          destOpts={{ tableName: "microtransactions" }}
+        />
+        <SnapshottedTable
+          sourcePlugin="parquet"
+          sourceOpts={{ fileURL: "https://link-to-parquet.test" }}
+          destOpts={{ tableName: "marketing_emails" }}
+        />
+        <QueryTable>
+          SELECT cookie_id, avg(count(microtransactions)) FROM microtransactions
+          LEFT JOIN marketing_emails ON cookie_id GROUP BY cookie_id;
+        </QueryTable>
+      </DataContext>
+    </div>
+  );
 };
 
 render(<DsxThingRoot />, {});
