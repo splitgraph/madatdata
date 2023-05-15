f52749b38234121af814a0a486ef927fc2aa070c
Miles Richardson <miles@splitgraph.com>
2023-05-16 00:52:38 +0100

    Create `AirbyteGitHubImportPlugin` as proof-of-concept of auto-generatd plugin
    This is currently manually created, but the idea is that a file like
    this will be auto-generated for each plugin. WIP

---
+++ packages/db-splitgraph/plugins/importers/airbyte-github-plugin.ts
@@ -0,0 +1,40 @@
+import type { AirbyteGithubParamsSchema } from "./generated/airbyte-github/ParamsSchema";
+import type { AirbyteGithubTableParamsSchema } from "./generated/airbyte-github/TableParamsSchema";
+import type { AirbyteGithubCredentialsSchema } from "./generated/airbyte-github/CredentialsSchema";
+
+import { makeGeneratedImportPlugin } from "./splitgraph-generated-import-plugin";
+
+import type { ImportDestOptions } from "./base-import-plugin";
+// import { SplitgraphImportCSVPlugin } from "./splitgraph-import-csv-plugin";
+
+export const AirbyteGithubImportPlugin = makeGeneratedImportPlugin<
+  "airbyte-github",
+  AirbyteGithubParamsSchema,
+  AirbyteGithubTableParamsSchema,
+  AirbyteGithubCredentialsSchema,
+  ImportDestOptions<
+    AirbyteGithubTableParamsSchema,
+    AirbyteGithubCredentialsSchema
+  >,
+  { params: AirbyteGithubParamsSchema }
+>("airbyte-github");
+
+// const plugin = new AirbyteGithubImportPlugin({
+//   graphqlEndpoint: "http://localhost:8080/v1alpha1/graphql",
+//   transformRequestHeaders: (headers) => headers,
+// });
+
+// const xxx = plugin.__name;
+
+// await plugin.importData({params: { }})
+
+// await plugin.importData()
+
+// plugin.importData()
+
+// const csvplugin = new SplitgraphImportCSVPlugin({
+//   graphqlEndpoint: "http://localhost:8080/v1alpha1/graphql",
+//   transformRequestHeaders: (headers) => headers,
+// });
+
+// plugin.importData("")
