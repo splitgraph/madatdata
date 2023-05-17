d1bd3b6d267d655c9254c1892291c6ae69f2bded
Miles Richardson <miles@splitgraph.com>
2023-05-17 21:51:42 +0100

    Add `SplitgraphExportToSeafowlPlugin` for exporting from Splitgraph to Seafowl
    We already supported "manually" exporting from Splitgraph to Seafowl
    by first exporting a `.parquet` file via the existing `ExportQueryPlugin`,
    and then importing it into Seafowl with `SeafowlImportFilePlugin`.
    
    However, the Splitgraph API supports the more robust use case of
    exporting one or multiple queries, tables and VDBs from Splitgraph
    to Seafowl, via the GraphQL mutation `exportToSeafowl` which is
    available with an authenticated request to the GraphQL API.
    
    This commit adds a plugin to call that API with one or multiple queries,
    tables or VDBs (with the types being fairly raw and maybe a bit cumbersome,
    albeit still equipped with full autocomplete and sufficient TypeDoc annotations),
    and to wait for the tasks to resolve before returning an object containing
    the result, including a list of passed jobs, and if applicable, a list of
    any failed jobs (since each export target creates a separate job, we need
    to wait for them separately, and each can fail or pass independently of the others).
    
    The result type is a bit of a mess, but it doesn't actually matter much
    since you don't really need to do anything with it, assuming error
    is not `null`, because that means the export was successful and you can
    proceed by simply querying the resultant data in Seafowl.

---
+++ packages/db-splitgraph/plugins/exporters/splitgraph-export-to-seafowl-plugin.ts
@@ -0,0 +1,227 @@
+import { gql } from "graphql-request";
+import type { ExportPlugin } from "@madatdata/base-db/base-db";
+import { SplitgraphExportPlugin } from "./base-export-plugin";
+import type { WithOptionsInterface } from "@madatdata/base-db/plugin-bindings";
+import type {
+  SeafowlExportQuerySourceDestinationTupleInput,
+  SeafowlExportTableSourceDestinationTupleInput,
+  SeafowlExportVdbSourceDestinationTupleInput,
+  SeafowlInstanceExportInput,
+} from "../../gql-client/unified-types";
+import type {
+  StartExportToSeafowlJobMutation,
+  StartExportToSeafowlJobMutationVariables,
+} from "./splitgraph-export-to-seafowl-plugin.generated";
+
+// TODO: manually expand these types so we can add more informative typedocs to them
+type ExportToSeafowlSourceOptions = {
+  tables?: ExpandRecursively<SeafowlExportTableSourceDestinationTupleInput>[];
+  queries?: ExpandRecursively<SeafowlExportQuerySourceDestinationTupleInput>[];
+  vdbs?: ExpandRecursively<SeafowlExportVdbSourceDestinationTupleInput>[];
+};
+
+type ExportToSeafowlDestOptions = {
+  seafowlInstance?: ExpandRecursively<SeafowlInstanceExportInput>;
+};
+
+type JobResult = Awaited<
+  ReturnType<(typeof SplitgraphExportPlugin)["prototype"]["waitForTask"]>
+>;
+
+export class SplitgraphExportToSeafowlPlugin
+  extends SplitgraphExportPlugin<
+    "export-to-seafowl",
+    SplitgraphExportToSeafowlPlugin,
+    ExportToSeafowlSourceOptions,
+    ExportToSeafowlDestOptions,
+    Record<string, unknown>,
+    Awaited<ReturnType<ExportPlugin<"exportQuery">["exportData"]>>
+  >
+  implements
+    ExportPlugin<"export-to-seafowl">,
+    WithOptionsInterface<SplitgraphExportToSeafowlPlugin>
+{
+  public readonly __name = "export-to-seafowl";
+  public static readonly __name = "export-to-seafowl";
+
+  async exportData(
+    sourceOptions: ExportToSeafowlSourceOptions,
+    destOptions: ExportToSeafowlDestOptions
+  ) {
+    const {
+      response: startExportResponse,
+      error: startExportError,
+      info: startExportInfo,
+    } = await this.startExport(sourceOptions, destOptions);
+
+    // Bail out if error, or response missing, or any fields missing from response
+    // Note that the fields might be empty lists, but we still expect them to be set
+    if (
+      startExportError ||
+      !startExportResponse ||
+      !startExportResponse.exportToSeafowl.queries ||
+      !startExportResponse.exportToSeafowl.tables ||
+      !startExportResponse.exportToSeafowl.vdbs
+    ) {
+      return {
+        response: startExportResponse ?? null,
+        error: startExportError ?? null,
+        info: {
+          ...startExportInfo,
+        },
+      };
+    }
+
+    const {
+      queries: queryExportJobs,
+      tables: tableExportJobs,
+      vdbs: vdbExportJobs,
+    } = startExportResponse.exportToSeafowl;
+
+    const passedJobs: {
+      queries: {
+        job: (typeof queryExportJobs)[number];
+        result: JobResult;
+      }[];
+      tables: {
+        job: (typeof tableExportJobs)[number];
+        result: JobResult;
+      }[];
+      vdbs: {
+        job: (typeof vdbExportJobs)[number];
+        result: JobResult;
+      }[];
+    } = {
+      queries: [],
+      tables: [],
+      vdbs: [],
+    };
+
+    const failedJobs: typeof passedJobs = {
+      queries: [],
+      tables: [],
+      vdbs: [],
+    };
+
+    const queryExportPromises = queryExportJobs.map((job) =>
+      this.waitForTask(job.jobId).then((result) =>
+        (result.response?.status === "SUCCESS"
+          ? passedJobs
+          : failedJobs
+        ).queries.push({ job, result })
+      )
+    );
+    const tableExportPromises = tableExportJobs.map((job) =>
+      this.waitForTask(job.jobId).then((result) =>
+        (result.response?.status === "SUCCESS"
+          ? passedJobs
+          : failedJobs
+        ).tables.push({ job, result })
+      )
+    );
+    const vdbExportPromises = vdbExportJobs.map((job) =>
+      this.waitForTask(job.jobId).then((result) =>
+        (result.response?.status === "SUCCESS"
+          ? passedJobs
+          : failedJobs
+        ).vdbs.push({ job, result })
+      )
+    );
+
+    const allPromises = [
+      ...queryExportPromises,
+      ...tableExportPromises,
+      ...vdbExportPromises,
+    ];
+
+    await Promise.all(allPromises);
+
+    const totalPassed =
+      passedJobs.queries.length +
+      passedJobs.tables.length +
+      passedJobs.vdbs.length;
+    const totalFailed =
+      failedJobs.queries.length +
+      failedJobs.tables.length +
+      failedJobs.vdbs.length;
+
+    return {
+      response: {
+        success: true, // FIXME: seems unnecessary, just here for consistency with other (export) plugins
+        passedJobs,
+      },
+      error:
+        totalFailed > 0
+          ? {
+              error: `${totalFailed} / ${
+                totalFailed + totalPassed
+              } jobs failed`,
+              failedJobs,
+            }
+          : null,
+      info: {
+        allPassed: totalFailed === 0, // note: true also if no jobs were run
+        somePassed: totalFailed > 0 && totalPassed > 0,
+        allFailed: totalPassed === 0 && totalFailed > 0,
+        totalPassed,
+        totalFailed,
+        startExportInfo,
+      },
+    };
+  }
+
+  protected async startExport(
+    sourceOptions: ExportToSeafowlSourceOptions,
+    destOptions: ExportToSeafowlDestOptions
+  ) {
+    return this.graphqlClient.send<
+      StartExportToSeafowlJobMutation,
+      StartExportToSeafowlJobMutationVariables
+    >(
+      gql`
+        mutation StartExportToSeafowlJob(
+          $queries: [SeafowlExportQuerySourceDestinationTupleInput!] = null
+          $tables: [SeafowlExportTableSourceDestinationTupleInput!] = null
+          $vdbs: [SeafowlExportVDBSourceDestinationTupleInput!] = null
+          $seafowlInstance: SeafowlInstanceExportInput! = { selfHosted: null }
+        ) {
+          exportToSeafowl(
+            queries: $queries
+            tables: $tables
+            vdbs: $vdbs
+            seafowlInstance: $seafowlInstance
+          ) {
+            tables {
+              jobId
+              sourceVdbId
+              sourceNamespace
+              sourceRepository
+              sourceTable
+            }
+            queries {
+              jobId
+              sourceVdbId
+              sourceQuery
+            }
+            vdbs {
+              jobId
+              sourceVdbId
+            }
+          }
+        }
+      `,
+      {
+        queries: sourceOptions.queries ?? [],
+        tables: sourceOptions.tables ?? [],
+        vdbs: sourceOptions.vdbs ?? [],
+        seafowlInstance: destOptions.seafowlInstance ?? { selfHosted: null },
+      }
+    );
+  }
+}
+
+export type ExpandRecursively<T> = T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: ExpandRecursively<O[K]> }
+    : never
+  : T;
